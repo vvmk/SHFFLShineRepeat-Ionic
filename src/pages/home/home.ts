c6f90c587e2dd2f0f8e17f93b34c0641264c7384
vvmk <vincentmasiello@gmail.com>
2018-05-03 13:50:32 -0400

    Timeouts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,19 +7,46 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
     public timeout: number;
+    public currentTitle: string = "";
+    public currentTick: number = 0;
+    public drills: any;
+    public drillIndex: number = 0;
 
     constructor(public navCtrl: NavController) {
-
+        this.drills = [
+        {
+            title: "First Drill",
+            duration: 10
+        },
+        {
+            title: "Second Drill",
+            duration: 5
+        }];
     }
 
-    startTimer(seconds: number): void {
-        this.tick(seconds);
-        if (seconds > 0) {
-            setTimeout(() => this.startTimer(seconds - 1), 1000);
+    runDrills() {
+        for (let drill of this.drills) {
+            this.currentTitle = drill.title;
+            this.startTimer(drill.duration);
+
         }
     }
 
+    startTimer(seconds: number): void {
+        this.currentTitle = this.drills[this.drillIndex].title;
+        this.tick(seconds);
+        if (seconds > 0) {
+            setTimeout(() => this.startTimer(seconds - 1), 1000);
+        } else {
+            if (this.drillIndex < this.drills.length) {
+                this.drillIndex++;
+                setTimeout(() => this.startTimer(this.drills[this.drillIndex].duration), 1000);
+            }
+        }
+    }
+    
+
     tick(secondsLeft: number): void {
-        console.log(secondsLeft);
+        this.currentTick = secondsLeft;
     }
 }
