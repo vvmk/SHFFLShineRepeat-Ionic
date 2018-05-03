18196d0963c187fce4c53f97a37b50d5ccd9c37c
vvmk <vincentmasiello@gmail.com>
2018-05-03 13:17:35 -0400

    Countdown proof of concept

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,24 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
+    public timeout: number;
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController) {
 
-  }
+    }
 
+    startTimer(seconds: number): void {
+        this.tick(seconds);
+        if (seconds > 0) {
+            setTimeout(() => this.startTimer(seconds - 1), 1000);
+        }
+    }
+
+    tick(secondsLeft: number): void {
+        console.log(secondsLeft);
+    }
 }
